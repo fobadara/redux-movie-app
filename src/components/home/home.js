b0274653bfa81879e399b29e53c40994989701fd
Abdulfatai <fob90s@gmail.com>
2022-02-03 11:44:45 +0100

    Write code to get search for movies in API

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,21 @@
-const Home = () => (
-  <div>Home</div>
-);
+import MovieListing from '../movie-listing/movie-listing';
+import movieApi from '../../common/apis/api';
+import key from '../../common/apis/api-key';
+
+const Home = () => {
+  const movieText = 'Harry';
+  const fetchMovies = async () => {
+    const response = await movieApi
+      .get(`?apikey=${key}&s=${movieText}&type=movie`)
+      .catch((err) => console.log('The response from the API:', err));
+    console.log(response);
+  };
+  fetchMovies();
+  return (<div>
+    <div className="banner-img"></div>
+    <MovieListing />
+  </div>
+  )
+};
 
 export default Home;
